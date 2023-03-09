9123b89e6d12ecc8a0f36bcc45c5b55d2af8f5c4
Maks Sorokin <mksmsrkn@gmail.com>
2023-03-09 10:55:58 -0500

    upd video.js

--- js/video.js
+++ js/video.js
@@ -3,11 +3,7 @@ const videos = document.querySelectorAll('.no-controls-video');
 
 // Loop through each video element and add event listeners
 videos.forEach(function(video) {
-  // Wait for the video to load
-  video.addEventListener('loadedmetadata', function() {
-    // Hide the video controls
-    video.controls = false;
-  });
+  video.controls = false;
 
   // Add event listeners to toggle the controls
   video.addEventListener('mouseover', function() {
